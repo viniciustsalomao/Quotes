924f737db57bd578b6e3ef30200a954f1032bf89
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-08-31 22:33:21 -0300

    Criação do app

---
+++ Pensamentos/Models/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  Pensamentos
+//
+//  Created by Vinícius Tinajero Salomão on 31/08/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import Foundation
+
+struct Quote {
+    
+}
---
+++ Pensamentos/Models/QuotesManager.swift
@@ -0,0 +1,9 @@
+//
+//  QuotesManager.swift
+//  Pensamentos
+//
+//  Created by Vinícius Tinajero Salomão on 31/08/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import Foundation
