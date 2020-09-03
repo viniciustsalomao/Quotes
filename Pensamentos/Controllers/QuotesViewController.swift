d945ac688c2dcfde0d7023dc1a4662703c712832
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-09-03 20:30:22 -0300

    Finalização do app

--- Pensamentos/Controllers/QuotesViewController.swift
+++ Pensamentos/Controllers/QuotesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class QuotesViewController: UIViewController {
-
+    
     @IBOutlet weak var ivPhoto: UIImageView!
     @IBOutlet weak var ivPhotoBg: UIImageView!
     @IBOutlet weak var lbQuote: UILabel!
@@ -19,25 +19,39 @@ class QuotesViewController: UIViewController {
     let quotesManager = QuotesManager()
     var timer: Timer?
     
+    let config = Configuration.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "Refresh"), object: nil, queue: nil) { (notification) in
+            self.formatView()
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        prepareQuote()
+        formatView()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         prepareQuote()
     }
-
+    
+    func formatView() {
+        view.backgroundColor = config.colorScheme == 0 ? .white : UIColor(red: 156.0/255.0, green: 68.0/255.0, blue: 15.0/255.0, alpha: 1.0)
+        lbQuote.textColor = config.colorScheme == 0 ? .black : .white
+        lbAuthor.textColor = config.colorScheme == 0 ? UIColor(red: 192.0/255.0, green: 96.0/255.0, blue: 49.0/255.0, alpha: 1.0) : .yellow
+        prepareQuote()
+    }
+    
     func prepareQuote() {
         timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: 8.0, repeats: true) { (timer) in
-            self.showRandomQuote()
+        if config.autorefresh == true {
+            
+            timer = Timer.scheduledTimer(withTimeInterval: config.timeInterval, repeats: true) { (timer) in
+                self.showRandomQuote()
+            }
         }
         showRandomQuote()
     }
